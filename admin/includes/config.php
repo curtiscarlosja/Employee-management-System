0bc7f2d825600b4fa392e51e801bc88d38afa5f0
CarlosCurtis <55671608+yaadbwoy@users.noreply.github.com>
2019-12-15 21:33:26 -0500

    remote connection

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,10 +1,23 @@
 <?php 
 // DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','elms');
+//define('DB_HOST','localhost');
+//define('DB_USER','root');
+//define('DB_PASS','');
+//define('DB_NAME','elms');
 // Establish database connection.
+
+
+
+//DB credentials.
+define('DB_HOST','remotemysql.com');
+define('DB_USER','Mq0byhlWiS');
+define('DB_PASS','mcn3dGCJsx');
+define('DB_NAME','elms');
+
+
+//$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+
 try
 {
 $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
