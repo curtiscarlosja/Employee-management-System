c6a493b9dacfd0f449c763fb2497c395f693ca7c
curtiscarlosja <56280651+curtiscarlosja@users.noreply.github.com>
2019-12-15 22:10:49 -0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -5,6 +5,14 @@ define('DB_USER','root');
 define('DB_PASS','');
 define('DB_NAME','elms');
 // Establish database connection.
+
+// Establish database remote DB connection.
+define('DB_HOST','remotemysql.com');
+define('DB_USER','Mq0byhlWiS');
+define('DB_PASS','mcn3dGCJsx');
+define('DB_NAME','Mq0byhlWiS');
+
+
 try
 {
 $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
